d9ed1cf8ae03e6f2667ad52b28eedd214b5c1067
medson <medsonnaftal@gmail.com>
2020-05-17 13:45:44 +0300

    Add login functionality on the system

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from django.contrib.auth.views import LoginView
 
-# Create your views here.
+
+class UserLoginView(LoginView):
+    """Allow user to login in platform """
+    template_name = 'registration/login.html'
+
+    def get_success_url(self):
+        url = self.get_redirect_url()
+        if url:
+            return url
+        elif self.request.user.is_farmer:
+            return reverse('farmers:farmer-homepage')
+        elif self.request.user.is_owner:
+            return reverse('owners:owner-homepage')
+        elif self.request.user.is_admin:
+            return reverse('admins:admin-homepage')
+        else:
+            return f'/admin/'
--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -92,7 +93,7 @@ if not DEBUG:
             },
         }
     }
-    
+
 else:
     DATABASES = {
         'default': {
