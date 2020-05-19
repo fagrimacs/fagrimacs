c323cb8d6c81643a93cf5dc9b9f5b2bde2f1d4f6
medson <medsonnaftal@gmail.com>
2020-05-19 10:39:23 +0300

    Add other fields on customer user models and authenticate password

--- accounts/forms.py
+++ accounts/forms.py
@@ -13,7 +13,7 @@ class UserCreationForm(forms.ModelForm):
 
     class Meta:
         model = CustomUser
-        fields = ('email', 'name',)
+        fields = ('name', 'email', 'phone', 'company', 'country', )
 
     def clean_password2(self):
         # Check that the two password entries match
@@ -21,6 +21,8 @@ class UserCreationForm(forms.ModelForm):
         password2 = self.cleaned_data.get("password2")
         if password1 and password2 and password1 != password2:
             raise forms.ValidationError("Passwords don't match")
+        if len(password1) < 8:
+            raise forms.ValidationError('Password too short, It must be 8 character or more')
         return password2
 
     def save(self, commit=True):
