c8963cdc7ee6979c623fc30557d67ca0ac3cf121
medson <medsonnaftal@gmail.com>
2020-05-26 12:24:33 +0300

    Add farmer and owner templates

--- admins/urls.py
+++ admins/urls.py
@@ -6,4 +6,6 @@ app_name = 'admins'
 
 urlpatterns = [
     path('', views.AdminHomeView.as_view(), name='admin-homepage'),
+    path('farmers/list', views.FarmersView.as_view(), name='farmer-list'),
+    path('owners/list', views.OwnersView.as_view(), name='owner-list'),
 ]
--- admins/views.py
+++ admins/views.py
@@ -4,3 +4,13 @@ from django.views.generic import TemplateView
 class AdminHomeView(TemplateView):
     """Add Farmer Dashboard view """
     template_name = 'admin/admin_home.html'
+
+
+class FarmersView(TemplateView):
+    """Add Farmer List view """
+    template_name = 'admin/farmers.html'
+
+
+class OwnersView(TemplateView):
+    """Add Owner List view """
+    template_name = 'admin/owners.html'
