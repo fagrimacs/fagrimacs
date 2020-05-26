14f1a60ea4b234c4e1dc31318d4716e3faf0d15c
medson <medsonnaftal@gmail.com>
2020-05-26 18:06:34 +0300

    Implement admin profile update

--- admins/urls.py
+++ admins/urls.py
@@ -6,6 +6,8 @@ app_name = 'admins'
 
 urlpatterns = [
     path('', views.AdminHomeView.as_view(), name='admin-homepage'),
+    path('profile/<int:pk>/', views.AdminProfileView.as_view(), name='admin-profile'),
+    path('profile/<int:pk>/update/', views.AdminProfileUpdateView.as_view(), name='admin-profile-update'),
     path('farmers/list', views.FarmersView.as_view(), name='farmer-list'),
     path('owners/list', views.OwnersView.as_view(), name='owner-list'),
 ]
--- admins/views.py
+++ admins/views.py
@@ -1,4 +1,12 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView, UpdateView
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin, UserPassesTestMixin
+)
+from django.contrib.messages.views import SuccessMessageMixin
+
+from .models import AdminProfile
+from .forms import AdminProfileUpdateForm
+from accounts.forms import UserUpdateForm
 
 
 class AdminHomeView(TemplateView):
@@ -14,3 +22,32 @@ class FarmersView(TemplateView):
 class OwnersView(TemplateView):
     """Add Owner List view """
     template_name = 'admin/owners.html'
+
+
+class ProfileView(TemplateView):
+    """Add Profile view """
+    template_name = 'admin/admin_profile.html'
+ 
+
+class AdminProfileView(DetailView):
+    model = AdminProfile
+    template_name = 'admin/admin_profile.html'
+    context_object_name = 'admin_profile'
+
+
+class AdminProfileUpdateView(LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin, UpdateView):
+    model = AdminProfile
+    form_class = AdminProfileUpdateForm
+    template_name = 'admin/adminprofile_update.html'
+    success_message = "Your profile has been updated successful."
+
+    def test_func(self):
+        if self.request.user.adminprofile == AdminProfile.objects.get(user_id=self.kwargs['pk']):
+            return True
+        return False
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_form'] = UserUpdateForm(instance=self.request.user)
+        context['profile_form'] = AdminProfileUpdateForm(instance=self.request.user.adminprofile)
+        return context
